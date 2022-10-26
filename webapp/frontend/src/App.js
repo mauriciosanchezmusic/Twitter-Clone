90deb79091bd75be97eb49169169c23046fd63d6
Citlalli27 <70440578+Citlalli27@users.noreply.github.com>
2022-10-26 10:59:20 -0500

    frontend main page

--- webapp/frontend/src/App.js
+++ webapp/frontend/src/App.js
@@ -3,6 +3,7 @@ import Home from "./components/Home";
 import Navbar from "./components/Navbar";
 import Login from "./components/Login";
 import Register from "./components/Register";
+import MainPage from "./components/MainPage";
 import { BrowserRouter as Router, Routes, Route, Link} from "react-router-dom";
 
 
@@ -15,6 +16,7 @@ function App() {
                     <Route exact path='/' element={< Home />}></Route>
                     <Route exact path='/login' element={< Login />}></Route>
                     <Route exact path='/register' element={< Register />}></Route>
+                    <Route exact path='/mainpage' element={< MainPage />}></Route>
                 </Routes>
             </Router>
         </React.Fragment>
