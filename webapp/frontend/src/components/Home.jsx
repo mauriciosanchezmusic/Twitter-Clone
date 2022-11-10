0e22a4064fb6652fb619c6a96c52f55f9a13d6f3
mauriciosanchezmusic <61812923+mauriciosanchezmusic@users.noreply.github.com>
2022-11-09 18:22:40 -0600

    Update Home.jsx
    Botones centrales funcionales y mejor personalizacion

--- webapp/frontend/src/components/Home.jsx
+++ webapp/frontend/src/components/Home.jsx
@@ -6,55 +6,24 @@ function Home() {
             <div
                 className="w3-container w3-center w3-blue"
                 style={{ padding: "2rem" }}>
-                <h1 className="w3-jumbo">TwitterClone</h1>
+                <h1 className="w3-jumbo" style={{border: "solid"}}>TwitterClone</h1>
+                <br></br><br></br><br></br><br></br><br></br><br></br>
                 <button
                     className="w3-button w3-pink"
-                    style={{ marginRight: "1rem" }}>
-                    Login
+                    style={{ marginRight: "1rem",fontSize:'50px' }}>
+                    <a  href="/login">
+                    Log In
+                    </a>
+
                 </button>
-                <button className="w3-button w3-pink">Register</button>
-            </div>
-
-            <div
-                className="w3-container w3-blue"
-                style={{ padding: "2rem", marginTop: "2rem" }}>
-                <h2>Lorem ipsum dolor sit amet</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Integer consectetur libero nibh, non sodales urna malesuada
-                    nec. Sed tortor eros, blandit eget fringilla at, gravida a
-                    nibh. Etiam dui nulla, aliquam vitae purus a, auctor
-                    malesuada arcu. Vestibulum venenatis orci nisl, sed
-                    elementum leo tincidunt eget. Nullam convallis nisi in.
-                </p>
-            </div>
-
-            <div
-                className="w3-container w3-blue"
-                style={{ padding: "2rem", marginTop: "2rem" }}>
-                <h2>Lorem ipsum dolor sit amet</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Integer consectetur libero nibh, non sodales urna malesuada
-                    nec. Sed tortor eros, blandit eget fringilla at, gravida a
-                    nibh. Etiam dui nulla, aliquam vitae purus a, auctor
-                    malesuada arcu. Vestibulum venenatis orci nisl, sed
-                    elementum leo tincidunt eget. Nullam convallis nisi in.
-                </p>
-            </div>
-
-            <div
-                className="w3-container w3-blue"
-                style={{ padding: "2rem", marginTop: "2rem" }}>
-                <h2>Lorem ipsum dolor sit amet</h2>
-                <p>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
-                    Integer consectetur libero nibh, non sodales urna malesuada
-                    nec. Sed tortor eros, blandit eget fringilla at, gravida a
-                    nibh. Etiam dui nulla, aliquam vitae purus a, auctor
-                    malesuada arcu. Vestibulum venenatis orci nisl, sed
-                    elementum leo tincidunt eget. Nullam convallis nisi in.
-                </p>
+                <br></br><br></br><br></br><br></br><br></br>
+                <button className="w3-button w3-pink"
+                style={{ marginRight: "1rem",fontSize:'50px' }}>
+                    <a  href="/register">
+                    Register
+                    </a>
+                </button>
+                <br></br><br></br><br></br><br></br>
             </div>
         </React.Fragment>
     );
