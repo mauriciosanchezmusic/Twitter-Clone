386927e8f68d6a0ecf197c25bfeea7abdcfd91d2
Citlalli27 <70440578+Citlalli27@users.noreply.github.com>
2022-11-13 17:22:26 -0600

    Button delete tweets

--- webapp/frontend/src/components/AddTweet.jsx
+++ webapp/frontend/src/components/AddTweet.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import {Editor} from "@tinymce/tinymce-react/lib/cjs/main/ts";
 
 function AddTweet() {
+
+    let [content, setContent] = React.useState("");
+
     return (<div className="w3-modal w3-animate-opacity" id="addTweet">
         <div className="w3-modal-content w3-card">
             <header className="w3-container w3-blue">
@@ -11,9 +15,32 @@ function AddTweet() {
             </header>
             <form className="w3-container">
                 <div className="w3-section">
-                    <label htmlFor="title">Title</label>
-                    <input type="text" id="title" className="w3-input w3-border w3-margin-bottom"/>
-                    <textarea cols="30" rows="10"/>
+                    <p>
+                        <label htmlFor="title">Title</label>
+                        <input type="text" id="title" className="w3-input w3-border w3-margin-bottom"/>
+                    </p>
+                    <Editor
+                        initialValue="<p>This is the initial content of the editor</p>"
+                        init={{
+                            height: 300,
+                            menubar: false,
+                            statusbar: false,
+                            toolbar_mode: "sliding",
+                            plugins: [
+                                'advlist autolink lists link image imagetools media emoticons preview anchor',
+                                'searchreplace visualblocks code fullscreen',
+                                'insertdatetime media table paste code help wordcount'
+                            ],
+                            toolbar:
+                                'undo redo | formatselect | bold italic underline strikethrough | image anchor media | \
+                                alignleft aligncenter alignright alignjustify | \
+                                outdent indent | bulllist numlist | fullscreen preview | emoticons help',
+                            contextmenu: "bold italic underline indent outdent help"
+                        }}
+                    />
+                    <p>
+                        <button type="submit" className="w3-button w3-blue">Post</button>
+                    </p>
                 </div>
             </form>
         </div>
--- webapp/frontend/src/components/TweetItem.jsx
+++ webapp/frontend/src/components/TweetItem.jsx
@@ -5,9 +5,12 @@ function TweetItem(props) {
         <div
             className="w3-card w3-border w3-border-gray w3-round-large"
             style={{ marginTop: "2rem" }}>
+            <header className="w3-container w3-opacity w3-light-gray" style={{padding: "1rem"}}>@{props.author}</header>
             <div className="w3-container" style={{ padding: "2rem" }}>
-                <h2 className="w3-opacity w3-xxlarge">{props.title}</h2>
-                <div dangerouslySetInnerHTML={{ __html: props.content }}></div>
+                <h2 className="w3-xxlarge">
+                    <span className="w3-opacity">{props.title}</span>
+                    <button className="w3-right w3-button w3-red w3-large w3-hover-pale-red w3-round-large">Delete</button></h2>
+                <div dangerouslySetInnerHTML={{__html: props.content}}/>
             </div>
             <footer className="w3-container w3-center w3-large">
                 <button className="w3-button" style={{ marginRight: "2rem" }}>
